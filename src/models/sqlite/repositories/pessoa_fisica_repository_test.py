e403af6d65e5827d31f86ed33935010f273442c7
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-21 18:58:11 -0300

    test: adding test from error in project

--- src/models/sqlite/repositories/pessoa_fisica_repository_test.py
+++ src/models/sqlite/repositories/pessoa_fisica_repository_test.py
@@ -1,4 +1,5 @@
 from unittest import mock
+from sqlalchemy.orm.exc import NoResultFound
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisicaTable
 from .pessoa_fisica_repository import PessoaFisicaRepository
@@ -22,6 +23,16 @@ class MockConnection:
     def __enter__(self): return self
     def __exit__(self, exc_type, exc_val, exc_tb): pass
 
+class MockConnectionNoResult:
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
+
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_val, exc_tb): pass
 
 def test_list_pessoa_fisica():
     mock_connection = MockConnection()
@@ -33,3 +44,14 @@ def test_list_pessoa_fisica():
     mock_connection.session.filter.assert_not_called()
 
     assert response[0].nome_completo== "fulano"
+
+def test_list_pessoa_fisica_no_result():
+    mock_connection = MockConnectionNoResult()
+    repo = PessoaFisicaRepository(mock_connection)
+    response = repo.list_pessoa_fisica()
+
+    mock_connection.session.query.assert_called_once_with(PessoaFisicaTable)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
