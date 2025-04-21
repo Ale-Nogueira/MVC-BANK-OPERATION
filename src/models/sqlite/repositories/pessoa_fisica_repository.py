49625a1acaf34c9d9f8913c0fa2094d0c3c98320
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-21 20:23:15 -0300

    feat: implementing pessoa_fisica_repository

--- src/models/sqlite/repositories/pessoa_fisica_repository.py
+++ src/models/sqlite/repositories/pessoa_fisica_repository.py
@@ -1,4 +1,5 @@
 from typing import List
+from sqlalchemy import REAL
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisicaTable
 
@@ -13,3 +14,39 @@ class PessoaFisicaRepository:
                 return pessoa_fisica
             except NoResultFound:
                 return []
+
+    def insert_pessoa_fisica(self,renda_mensal: REAL, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: REAL) -> None:
+        with self.__db_connection as databse:
+            try:
+                person_data = PessoaFisicaTable(
+                    renda_mensal=renda_mensal,
+                    idade=idade,
+                    nome_completo=nome_completo,
+                    celular=celular,
+                    email=email,
+                    categoria=categoria,
+                    saldo=saldo
+                )
+                databse.session.add(person_data)
+                databse.session.commit()
+            except Exception as exception:
+                databse.session.rollback()
+                raise exception
+
+    def get_person(self, person_id: int) -> PessoaFisicaTable:
+        with self.__db_connection as database:
+            try:
+                person = (
+                    database.session
+                        .query(PessoaFisicaTable)
+                        .filter(PessoaFisicaTable.id == person_id)
+                        .with_entities(
+                            PessoaFisicaTable.nome_completo,
+                            PessoaFisicaTable.idade,
+                            PessoaFisicaTable.renda_mensal
+                        )
+                        .one()
+                )
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -3,7 +3,7 @@ from src.models.sqlite.setting.connection import db_connection_handler
 from .pessoa_fisica_repository import PessoaFisicaRepository
 from.pessoa_juridica_repository import PessoaJuridicaRepository
 
-#db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="interacao com o banco")
 def test_list_pessoas_fisica():
@@ -16,5 +16,26 @@ def test_list_pessoas_fisica():
 def test_list_pessoas_juridica():
     repo = PessoaJuridicaRepository(db_connection_handler)
     response = repo.list_pessoa_juridica()
+    print()
+    print(response)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_insert_person():
+    renda_mensal = 5000
+    idade = 26
+    nome_completo = "fulano de tal"
+    celular = "119988554"
+    email = "email@test"
+    categoria = "categoria A"
+    saldo = 10000
+
+    repo = PessoaFisicaRepository(db_connection_handler)
+    repo.insert_pessoa_fisica(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
+
+def test_get_person():
+    person_id = 1
+
+    repo = PessoaFisicaRepository(db_connection_handler)
+    response = repo.get_person(person_id)
     print()
     print(response)
