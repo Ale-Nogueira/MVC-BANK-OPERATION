c34db2f1de9ab761cce901e4496c3fa8744ae4d2
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-25 14:16:14 -0300

    feat: implementing pessoa_juridica_controller with tests

---
+++ src/controllers/pessoa_juridica_controller.py
@@ -0,0 +1,58 @@
+from src.models.sqlite.interfaces.cliente_interface import ClienteInterface
+
+class PessoaJuridicaSaqueController:
+    def __init__(self, pessoa_juridica_repository: ClienteInterface) -> None:
+        self.__pessoa_juridica_repository = pessoa_juridica_repository
+
+    def sacar(self, saque_info: dict) -> dict:
+        person_id = saque_info["person_id"]
+        valor = saque_info["valor"]
+
+        self.__validate_id_and_valor(person_id, valor)
+        self.__sacar_dinheiro(person_id, valor)
+        formated_response = self.__format_response(saque_info)
+        return formated_response
+
+    def __validate_id_and_valor(self, person_id: int, valor: int) -> None:
+        if person_id is None or valor is None:
+            raise Exception("ID e valor são obrigatórios.")
+
+    def __sacar_dinheiro(self, person_id: int, valor: int) -> None:
+        self.__pessoa_juridica_repository.sacar_dinheiro(person_id,valor)
+
+    def __format_response(self, saque_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Saque",
+                "count": 1,
+                "attributes": saque_info
+            }
+        }
+
+class PessoaJuridicaExtratoController:
+    def __init__(self, pessoa_juridica_repository: ClienteInterface) -> None:
+        self.__pessoa_juridica_repository = pessoa_juridica_repository
+
+    def extrato(self, extrato_info: dict) -> None:
+        person_id = extrato_info["person_id"]
+
+        self.__validate_id(person_id)
+        self.__realizar_saque(person_id)
+        formated_response = self.__format_response(extrato_info)
+        return formated_response
+
+    def __validate_id(self, person_id: int) -> None:
+        if person_id is None:
+            raise Exception("ID obrigatório.")
+
+    def __realizar_saque(self, person_id: int) -> None:
+        self.__pessoa_juridica_repository.realizar_extrato(person_id)
+
+    def __format_response(self, extrato_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Extrato",
+                "count": 1,
+                "attributes": extrato_info
+            }
+        }
---
+++ src/controllers/pessoa_juridica_controller_test.py
@@ -0,0 +1,56 @@
+import pytest
+from .pessoa_juridica_controller import PessoaJuridicaSaqueController, PessoaJuridicaExtratoController
+
+
+class MockPessoaJuridica:
+    def sacar_dinheiro(self, person_id: int, valor: int):
+        pass
+
+    def realizar_extrato(self, person_id: int):
+        pass
+
+def test_saque():
+    saque_info = {
+        "person_id": 1,
+        "valor": 1000
+    }
+
+    controller = PessoaJuridicaSaqueController(MockPessoaJuridica())
+    response = controller.sacar(saque_info)
+
+    assert response["data"]["type"] == "Saque"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == saque_info
+
+def test_saque_error():
+    saque_info = {
+        "person_id": None,
+        "valor": 1000
+    }
+
+    controller = PessoaJuridicaSaqueController(MockPessoaJuridica())
+
+    with pytest.raises(Exception):
+        controller.sacar(saque_info)
+
+def test_extrato():
+    extrato_info = {
+        "person_id": 1
+    }
+
+    controller = PessoaJuridicaExtratoController(MockPessoaJuridica())
+    response = controller.extrato(extrato_info)
+
+    assert response["data"]["type"] == "Extrato"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == extrato_info
+
+def test_extrato_error():
+    extrato_info = {
+        "person_id": None
+    }
+
+    controller = PessoaJuridicaExtratoController(MockPessoaJuridica())
+
+    with pytest.raises(Exception):
+        controller.extrato(extrato_info)
