1477017ddbb0d8bca67f91a4790038fc513ea6a5
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-25 12:56:46 -0300

    feat: adding controllers with pessoa fisica

---
+++ src/controllers/pessoa_fisica_controller.py
@@ -0,0 +1,30 @@
+from src.models.sqlite.interfaces.cliente_interface import ClienteInterface
+
+class PessoaFisicaSaqueController:
+    def __init__(self, pessoa_fisica_repository: ClienteInterface) -> None:
+        self.__pessoa_fisica_repository = pessoa_fisica_repository
+
+    def sacar(self, saque_info: dict) -> dict:
+        person_id = saque_info.get("id")
+        valor = saque_info.get("valor")
+
+        self.__validate_id_and_valor(person_id, valor)
+        self.__sacar_dinheiro(person_id, valor)
+        formated_response = self.__format_response(saque_info)
+        return formated_response
+
+    def __validate_id_and_valor(self, person_id: int, valor: int) -> None:
+        if person_id is None or valor is None:
+            raise Exception("ID e valor são obrigatórios.")
+
+    def __sacar_dinheiro(self, person_id: int, valor: int) -> None:
+        self.__pessoa_fisica_repository.sacar_dinheiro(person_id,valor)
+
+    def __format_response(self, saque_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Saque",
+                "count": 1,
+                "attributes": saque_info
+            }
+        }
