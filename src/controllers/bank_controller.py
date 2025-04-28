7c9ce7d694b7cdc82d26f72a9720c42ca687b690
Alexandre Nogueira <alexandrenog2013@gmail.com>
2025-04-28 12:06:00 -0300

    feat:implementing bank_controller with tests

--- src/controllers/bank_controller.py
+++ src/controllers/bank_controller.py
@@ -1,8 +1,9 @@
 from typing import List
+import re
 from src.models.sqlite.interfaces.bank_interface import BankInterface
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisicaTable
 
-class BankListController:
+class BankListPessoaFisicaController:
     def __init__(self, bank_repository: BankInterface) -> None:
         self.__bank_repository = bank_repository
 
@@ -25,5 +26,41 @@ class BankListController:
                 "type": "Pessoa Fisica",
                 "count": len(formatted_pessoa_fisica),
                 "attributes": formatted_pessoa_fisica
+            }
+        }
+
+class BankInsertPessoaFisicaController:
+    def __init__(self, bank_repository: BankInterface) -> None:
+        self.__bank_repository = bank_repository
+
+    def insert(self, person_info: dict) -> dict:
+        renda_mensal=person_info["renda_mensal"]
+        idade=person_info["idade"]
+        nome_completo=person_info["nome_completo"]
+        celular=person_info["celular"]
+        email=person_info["email"]
+        categoria=person_info["categoria"]
+        saldo=person_info["saldo"]
+
+        self.__validade_nome_completo(nome_completo)
+        self.__insert_person_in_db(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
+        formated_response = self.__format_response(person_info)
+        return formated_response
+
+    def __validade_nome_completo(self, nome_completo: str) -> None:
+        non_valid_caracteres = re.compile(r'[^a-zA-Z\s]')
+
+        if non_valid_caracteres.search(nome_completo):
+            raise Exception("Nome da pessoa invalido!")
+
+    def __insert_person_in_db(self, renda_mensal= int, idade=int, nome_completo= str, celular= int, email= str, categoria= str, saldo= int) -> None:
+        self.__bank_repository.insert_pessoa_fisica(renda_mensal, idade, nome_completo, celular, email, categoria, saldo)
+
+    def __format_response(self, person_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "Pessoa Fisica",
+                "count": 1,
+                "attributes": person_info
             }
         }
--- src/controllers/bank_controller_test.py
+++ src/controllers/bank_controller_test.py
@@ -1,5 +1,6 @@
 from src.models.sqlite.entities.pessoa_fisica import PessoaFisicaTable
-from .bank_controller import BankListController
+from .bank_controller import BankListPessoaFisicaController
+from .bank_controller import BankInsertPessoaFisicaController
 
 class MockBankRepository:
     def list_pessoa_fisica(self):
@@ -8,8 +9,12 @@ class MockBankRepository:
             PessoaFisicaTable(nome_completo="ciclano de tal", idade=21, email="ciclano@email.com", id=2)
         ]
 
+    def insert_pessoa_fisica(self, renda_mensal: int, idade: int, nome_completo: str, celular: str, email: str, categoria: str, saldo: int) -> None:
+        pass
+
+
 def test_list_pessoa_fisica():
-    controller = BankListController(MockBankRepository())
+    controller = BankListPessoaFisicaController(MockBankRepository())
     response = controller.list_pessoa_fisica()
 
     expected_response = {
@@ -24,3 +29,21 @@ def test_list_pessoa_fisica():
     }
 
     assert response == expected_response
+
+def test_insert_pessoa_fisica():
+    person_info = {
+        "renda_mensal": 5000,
+        "idade": 25,
+        "nome_completo": "fulano de tal",
+        "celular": "99885544",
+        "email": "fulano@email.com",
+        "categoria": "categoria A",
+        "saldo": 3000
+    }
+
+    controller = BankInsertPessoaFisicaController(MockBankRepository())
+    response =  controller.insert(person_info)
+
+    assert response["data"]["type"] == "Pessoa Fisica"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == person_info
